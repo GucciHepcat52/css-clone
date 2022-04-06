64599709084d426138ce131fa6c4133a24df7a9c
GucciHepcat52 <dallinbreen624@gmail.com>
2022-04-05 19:57:52 -0600

    Added routes for practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './header/Header';
+import { Routes, Route } from "react-router-dom";
+import Services from "../components/Services";
+import Portfolio from "../components/Portfolio";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </div>
   );
 }
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Navbar.css";
 import "../../index.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="title">Start Bootstrap</div>
-            <ul>
-                <li>Services</li>
-                <li>Portfolio</li>
-                <li>About</li>
-                <li>Team</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <div className="title">Start Bootstrap</div>
+      <ul>
+        <li>
+          <Link to="/">Services</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>About</li>
+        <li>Team</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
